39453538c0302ede4f16be467dbdb5d5b55017c4
browseo <131001626+vincentformationopen@users.noreply.github.com>
2023-07-28 14:35:46 +0200

    ADD FORM,ADMIN IN MENU, COMMANDER PAGE

--- functions.php
+++ functions.php
@@ -21,4 +21,50 @@ if ( !function_exists( 'child_theme_configurator_css' ) ):
 endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
+function buttonorder() {
+
+    $monTexte = '<div class="parent">
+    <div class="div1"><input value="0"></div>
+    <div class="div2">+</div>
+    <div class="div3">-</div>
+    <div class="div4">OK</div>
+    </div>';
+    
+    return $monTexte;
+    }
+    
+    add_shortcode('nomShortcode', 'buttonorder');
+
 // END ENQUEUE PARENT ACTION
+
+/*
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in() && $args->menu === 'menutop') {
+        $items .= '<li><a href="'. get_permalink( get_option('woocommerce_myaccount_page_id') ) .'">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+*/
+
+
+add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
+
+
+function add_admin_link( $items, $args ) {
+
+
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+
+
+        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+
+
+    }
+
+
+    return $items;
+
+
+}
+
